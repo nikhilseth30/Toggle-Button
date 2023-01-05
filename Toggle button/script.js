7ea43befdbbf3eea06864590a637c96299b176c6
Nikhil seth <nikhilseth30@gmail.com>
2023-01-05 13:49:02 +0530

    toggle button

---
+++ Toggle button/script.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('.toggleBtn')
+const moveBtn = document.querySelector('.bgMove')
+const fa = document.querySelector('.fa-solid')
+const faMoon = document.querySelector('.fa-moon')
+
+btn.addEventListener('click', function () {
+    moveBtn.classList.toggle('active')
+
+    fa.classList.toggle('dark')
+
+    faMoon.classList.toggle('active')
+    if (moveBtn.classList == 'active') {
+        faMoon.classList.add('moon-active')
+    }
+    else {
+        faMoon.classList.remove('moon-active')
+    }
+   
+    if (moveBtn.classList.contains('active')) {
+        document.body.classList.add('dark')
+    }
+    else {
+        document.body.classList.remove('dark')
+    }
+})
